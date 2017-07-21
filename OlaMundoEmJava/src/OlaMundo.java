484bdfd912c2a426c7d98b1412d2d317fb999cf7
Marlon Ferreira dos Santos Silva <matheista@localhost.localdomain>
2017-07-21 16:19:33 -0300

    Parte do Roteiro1

---
+++ OlaMundoEmJava/src/OlaMundo.java
@@ -0,0 +1,13 @@
+
+public class OlaMundo {
+	public static void main(String [] args){
+		System.out.println("Ola Marlon");
+		System.out.println("Vamos faze umas continhas");
+		System.out.println(122+12);
+		System.out.println(50-12);
+		System.out.println(87*3);
+		System.out.println(78/12);
+	
+	}
+
+}
