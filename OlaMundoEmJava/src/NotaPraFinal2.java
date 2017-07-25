73c79106be238d47822fbe44cdfe4dfa9a6d508b
Marlon Ferreira dos Santos Silva <matheista@localhost.localdomain>
2017-07-25 09:26:01 -0300

    Roteiro da Aula1

--- OlaMundoEmJava/src/NotaPraFinal2.java
+++ OlaMundoEmJava/src/NotaPraFinal2.java
@@ -3,16 +3,10 @@ import javax.swing.JOptionPane;
 public  class NotaPraFinal2 {
 	public static void main(String [] args){
 		double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 1ª nota"));
-		JOptionPane.showMessageDialog(null, "A nota lida foi " +nota1);
 		double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 2ª nota"));
-		JOptionPane.showMessageDialog(null,"A nota lida foi "+nota2);
 		double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 3º nota"));
-		JOptionPane.showMessageDialog(null,"A nota lida foi " +nota3);
 		double media = (nota1+nota2+nota3)/3;
 		JOptionPane.showMessageDialog(null,"A média lida foi " +media);
-		
-
-		
 		
 		
 	}
