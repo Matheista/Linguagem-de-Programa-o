6f28a9e9e81f9ac73215ddd5554ba4a1e2d6ca1d
Marlon Ferreira dos Santos Silva <matheista@localhost.localdomain>
2017-07-25 09:23:58 -0300

    Roteiro de Aula1

--- OlaMundoEmJava/src/OlaMundo.java
+++ OlaMundoEmJava/src/OlaMundo.java
@@ -1,13 +1,7 @@
 
 public class OlaMundo {
-	public static void main(String [] args){
+	public static void main(String[] args){
 		System.out.println("Ola Marlon");
-		System.out.println("Vamos faze umas continhas");
-		System.out.println(122+12);
-		System.out.println(50-12);
-		System.out.println(87*3);
-		System.out.println(78/12);
-	
 	}
 
 }
